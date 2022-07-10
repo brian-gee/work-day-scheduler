649ab1436c9c2271a6242dbf6e59fd6f04d9246b
Brian Gee <briangeertsma@gmail.com>
2022-07-10 13:23:47 -0400

    display current date using moment.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,4 @@
-var d = new Date();
-var day = d.getDay();
-var month =  d.getMonth();
-var dayList = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-var monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-todaysDate = dayList[day] + ", " + monthList[month] + " " + d.getUTCDate();
-dateDisplay = document.querySelector("#currentDay")
-
-dateDisplay.innerHTML = todaysDate;
+// Code to display current date using moment
+var m = moment();
+var currentDay = document.querySelector("#currentDay");
+currentDay.innerHTML = m.format("dddd, MMMM Do");
