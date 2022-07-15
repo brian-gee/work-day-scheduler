5d7d4d6ac212ab6180037ab601b200bd8163daaf
Brian Gee <briangeertsma@gmail.com>
2022-07-14 21:56:05 -0400

    added ability to save input to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,15 @@
-// Code to display current date using moment
+// Display current date using moment
 var m = moment();
 var currentDay = document.querySelector("#currentDay");
 currentDay.innerHTML = m.format("dddd, MMMM Do");
 
-// Code to add or remove class based on time
+// Add or remove class based on time
 var hours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
 hours.forEach( (hour) => {
     blockToAssign = document.querySelector("#block-" + hour);
     classToAssign = ""
-
     currentHour = m.hour();
-    console.log(currentHour);
 
     if (hour < currentHour) {
         classToAssign = "past";
@@ -22,18 +20,19 @@ hours.forEach( (hour) => {
     }
 
     blockToAssign.classList.add(classToAssign);
+
+    // Assign localstorage value to textarea if it exists
+    if (localStorage["TextAreaData-" + hour]) {
+        blockToAssign.value = window.localStorage["TextAreaData-" + hour];
+    }    
 })
 
-// Code to make all saveBtn clickable
-var saveBtn = document.querySelectorAll(".saveBtn");
-saveBtn.forEach( (btn) => {
-    btn.addEventListener("click", saveInput);
-})
-
-// Function to save input entered into textarea
-function saveInput() {
-    alert("Woo!")
-    // var input = document.querySelector(".col")
-    // localStorage.setItem(key, input.value)
+// Make all saveBtn clickable and store textArea value to localstorage
+for (var i = 9; i < 18; i++) {
+    var saveBtn = document.querySelector("#btn-" + i);
+    saveBtn.addEventListener("click", (e) => {
+        var blockNum = e.target.id.substr(4);
+        blockTextArea = document.querySelector("#block-" + blockNum);
+      localStorage["TextAreaData-" + blockNum] = blockTextArea.value;
+    });
 }
-
